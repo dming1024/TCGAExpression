d8220d9dcfa287e3029438c6a0e6cfbbf07f8ec0
fanqiang1024 <dongliulou@gmail.com>
2022-07-16 11:36:00 +0800

    v2.0

--- R/getExpression.R
+++ R/getExpression.R
@@ -9,26 +9,45 @@
 #'
 #' @examples
 #' library(TCGAExpression)#'
-#' inputToken("yJ0eXAiOiJKV1QiLCJhbGciOiJI...xNjUzODMyOTAyLjM4NTU3MzksImlzcyI6Inl4d")#update your token
 #' getExpression('TP53','TCGA-LUAD')
-getExpression<-function(gene,Project){
-  token=getToken()
-  if(length(token)==0){
-    warning("Token已过期!请先使用: inputToken(yourToken) 更新token\n")
-    df=data.frame()
-  }else{
-    url=sprintf("http://yixuetongji.top:8000/Gene/%s/%s?Project=%s",
-                gene,as.character(token),Project)
-    raw_result <- httr::GET(url)
-    my_content <- httr::content(raw_result,type="application/json")
-    if(length(my_content)>2){
-      df<-data.frame("gene"=unlist(my_content$TPM),
-                     "Group"=unlist(my_content$Group))
-    }else{
-      warning("Token已过期!请先使用: inputToken(yourToken) 更新token\n")
-      df=data.frame()
-    }
+#'
+
+getExpression <- function(gene, Project) {
+  url = sprintf(
+    "http://yixuetongji.top:8000/Gene/%s?Project=%s",
+    gene,
+    Project
+  )
+  raw_result <- httr::GET(url)
+  my_content <- httr::content(raw_result, type = "application/json")
+  if (length(my_content) > 2) {
+    df <- data.frame("gene" = unlist(my_content$TPM),
+                     "Group" = unlist(my_content$Group))
+  } else{
+    warning("输入格式有误，请核对后重新输入\n")
+    df = data.frame()
   }
 
   return(df)
 }
+# getExpression<-function(gene,Project){
+#   token=getToken()
+#   if(length(token)==0){
+#     warning("Token已过期!请先使用: inputToken(yourToken) 更新token\n")
+#     df=data.frame()
+#   }else{
+#     url=sprintf("http://yixuetongji.top:8000/Gene/%s/%s?Project=%s",
+#                 gene,as.character(token),Project)
+#     raw_result <- httr::GET(url)
+#     my_content <- httr::content(raw_result,type="application/json")
+#     if(length(my_content)>2){
+#       df<-data.frame("gene"=unlist(my_content$TPM),
+#                      "Group"=unlist(my_content$Group))
+#     }else{
+#       warning("Token已过期!请先使用: inputToken(yourToken) 更新token\n")
+#       df=data.frame()
+#     }
+#   }
+#
+#   return(df)
+# }
--- README.md
+++ README.md
@@ -3,5 +3,41 @@
 ## 目的
 可视化gene 在TCGA 各项目中的表达。
 
-## 获取Token
-可以从这里获取token：http://yixuetongji.top/TCGAToken/ 。
+## 使用方法
+
+主要有以下3中功能：
+
++ 获取基因表达
+
++ 对基因表达进行可视化
+
++ 获取TCGA项目
+
+### 获取基因表达
+
+```R
+library(TCGAExpression)
+
+df=getExpression('TP53','TCGA-LUAD')
+
+head(df)
+```
+
+
+### 基因表达可视化
+
+在`tumor` 和 `normal`组，分别可视化基因的表达
+
+```R
+getBoxplot(df)
+```
+
+<img src=".figures/1.png" />
+
+
+### 获取TCGA项目
+
+```R
+getProjects()
+```
+<img src=".figures/1.png" />
